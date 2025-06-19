f7905bff9636ad1c96beef88305a33a853fa6bb2
Timur Harin <darktimka007@gmail.ru>
2025-06-19 15:35:05 +0300

    lab1 complete

--- labs/lab01/frontend/lib/counter_app.dart
+++ labs/lab01/frontend/lib/counter_app.dart
@@ -10,21 +10,64 @@ class CounterApp extends StatefulWidget {
 class _CounterAppState extends State<CounterApp> {
   int _counter = 0;
 
-  void _increment() {
-    // TODO: Implement increment
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
   }
 
-  void _decrement() {
-    // TODO: Implement decrement
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
   }
 
-  void _reset() {
-    // TODO: Implement reset
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Implement counter UI
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter App'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: _resetCounter,
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$_counter',
+              style: const TextStyle(fontSize: 48),
+            ),
+            const SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 32),
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
