15e5c19f1f4f17572c965f3cb83d5d0a88353e30
Amir Avhadiev <almir.avhadiev@gmail.com>
2025-06-23 08:15:07 +0300

    Implement frontend part.

--- labs/lab01/frontend/lib/counter_app.dart
+++ labs/lab01/frontend/lib/counter_app.dart
@@ -11,20 +11,45 @@ class _CounterAppState extends State<CounterApp> {
   int _counter = 0;
 
   void _increment() {
-    // TODO: Implement increment
+    setState(() {
+      _counter++;
+    });
   }
 
   void _decrement() {
-    // TODO: Implement decrement
+    setState(() {
+      _counter--;
+    });
   }
 
   void _reset() {
-    // TODO: Implement reset
+    setState(() {
+      _counter = 0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Implement counter UI
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Counter")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('$_counter', style: const TextStyle(fontSize: 24)),
+                IconButton(
+                    icon: const Icon(Icons.remove), onPressed: _decrement),
+                IconButton(icon: const Icon(Icons.add), onPressed: _increment),
+                IconButton(icon: const Icon(Icons.refresh), onPressed: _reset),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
