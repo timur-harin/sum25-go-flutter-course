7763241d8e09e26e3a55e23ab1c85f635171f19e
fenton512 <viktorken8264@gmail.com>
2025-07-01 23:18:19 +0300

    implement 3 flutter widjets
    -counter app that represents another page with buttons for increment and decrement counter, and text for shows current value of counter
    -profile_card to shows information about user like name, email and age
    -registration form with basic validation input and 3 fields: name, password and email

--- labs/lab01/frontend/lib/main.dart
+++ labs/lab01/frontend/lib/main.dart
@@ -32,7 +32,6 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
