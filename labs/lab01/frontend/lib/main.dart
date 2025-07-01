d35e5d15c0c4215a0a81ca973ed9f8c4114a4b88
tdzdslippen <ars.laptev@innopolis.university>
2025-07-01 14:14:14 +0300

    lab01: frontend tasks

--- labs/lab01/frontend/lib/main.dart
+++ labs/lab01/frontend/lib/main.dart
@@ -10,27 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lab 01 Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
@@ -44,45 +28,36 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('Lab 01 Demo'),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Lab 01 Demo'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Profile'),
+              Tab(text: 'Counter'),
+              Tab(text: 'Register'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
           children: [
-            const Text(
-              'Profile Card Example',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            Center(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(16.0),
+                child: ProfileCard(
+                  name: 'Arseniy Laptev',
+                  email: 'ars.laptev@innopolis.university',
+                  age: 18,
+                  avatarUrl:
+                  'https://pic.rutubelist.ru/user/48/d0/48d0bb5d2ec422075b97263eb856ae8c.jpg',
+                ),
+              ),
             ),
-            const SizedBox(height: 8),
-            const ProfileCard(
-              name: 'John Doe',
-              email: 'john@example.com',
-              age: 30,
-            ),
-            const SizedBox(height: 24),
-            const Text(
-              'Counter App Example',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const CounterApp(),
-            const SizedBox(height: 24),
-            const Text(
-              'Registration Form Example',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const RegistrationForm(),
+            CounterApp(),
+            RegistrationForm(),
           ],
         ),
       ),
