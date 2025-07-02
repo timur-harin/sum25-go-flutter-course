ed95fb996a7304a08baa716e521c8f11c212a561
Timur <timyr20060430@gmail.com>
2025-07-02 03:02:09 +0300

    lab01: Timur Kagarmanov
    - add the implementation of algebraic operations and the ability to switch between data types to the calculator
    
    - add the implementation of taskmanager for adding, deleting, editing, and getting tasks, as well as for getting all tasks and launching the manager
    
    - add a user handler for adding users, checking data validity, updating data, and returning information about them
    
    - add a buttons and methods for adding, subtracting, and resetting the displayed number
    
    - add a user profile for displaying general information about the user and their photo
    
    - add a form for registering a new user with data validation
    
    - I made changes to the metadata and reverted them to the code from the original repository

--- labs/lab01/frontend/lib/profile_card.dart
+++ labs/lab01/frontend/lib/profile_card.dart
@@ -24,15 +24,43 @@ class ProfileCard extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             // TODO: add a CircleAvatar with radius 50 and backgroundImage NetworkImage(avatarUrl!) if url is not null and text name[0].toUpperCase() if url is null
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.blue[200],
+              backgroundImage: (avatarUrl == null || avatarUrl!.isEmpty) ? null : NetworkImage(avatarUrl!),
+              child: (avatarUrl == null || avatarUrl!.isEmpty) ? (Text((name.isEmpty) ? "?" : name[0].toUpperCase())) : null,
+            ),
             
             const SizedBox(height: 16),
             // TODO: add a Text with name and style fontSize: 24, fontWeight: FontWeight.bold
+            Text(
+              name,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
            
             const SizedBox(height: 8),
             // TODO: add a Text with Age: $age and style fontSize: 16
+            Text(
+              'Age: $age',
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
            
             const SizedBox(height: 8),
             // TODO: add a Text with email and style fontSize: 16, color: Colors.grey
+            Text(
+              email,
+              style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+              ),
+            ),
             
           ],
         ),
