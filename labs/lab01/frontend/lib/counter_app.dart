7d303b92caaf8682a741fae91b6725cc5bbadfe1
Molark <botalov2006egor@gmail.com>
2025-07-02 14:05:46 +0300

    lab01: tasks solutions  - Add backend and frontend solutions  - finish  Calculator TaskManager and User  - Finish CounterApp ProfileCard and Registration form  - Pass all unit  tests for each task

--- labs/lab01/frontend/lib/counter_app.dart
+++ labs/lab01/frontend/lib/counter_app.dart
@@ -1,58 +1,80 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-class CounterApp extends StatefulWidget {
-  const CounterApp({Key? key}) : super(key: key);
+  class CounterApp extends StatefulWidget {
+    const CounterApp({Key? key}) : super(key: key);
 
-  @override
-  State<CounterApp> createState() => _CounterAppState();
-}
-
-class _CounterAppState extends State<CounterApp> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    // TODO: Implement this function
+    @override
+    State<CounterApp> createState() => _CounterAppState();
   }
 
-  void _decrementCounter() {
-    // TODO: Implement this function
-  }
+  class _CounterAppState extends State<CounterApp> {
+    int _counter = 0;
 
-  void _resetCounter() {
-    // TODO: Implement this function
-  }
+    void _incrementCounter() {
+      // TODO: Implement this function
+      setState(() {
+        _counter+=1;
+      });
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter App'),
-        actions: [
-          // TODO: add a refresh button with Icon(Icons.refresh)
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 48),
-            ),
-            const SizedBox(height: 32),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // TODO: add a decrement button with Icon(Icons.remove) and onPressed: _decrementCounter
-                
-                const SizedBox(width: 32),
-                // TODO: add a increment button with Icon(Icons.add) and onPressed: _incrementCounter
-                
-              ],
+    void _decrementCounter() {
+      // TODO: Implement this function
+      setState(() {
+        _counter-=1;
+      });
+    }
+
+    void _resetCounter() {
+      // TODO: Implement this function
+      setState(() {
+        _counter=0;
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter App'),
+          actions: [
+            
+            // TODO: add a refresh button with Icon(Icons.refresh)
+            IconButton(
+              onPressed: _resetCounter,
+              icon: const Icon(Icons.refresh),
             ),
           ],
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$_counter',
+                style: const TextStyle(fontSize: 48),
+              ),
+              const SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // TODO: add a decrement button with Icon(Icons.remove) and onPressed: _decrementCounter
+                  FloatingActionButton(
+              onPressed: _decrementCounter,
+              child: const Icon(Icons.remove),
+            ),
+                  
+                  const SizedBox(width: 32),
+                  // TODO: add a increment button with Icon(Icons.add) and onPressed: _incrementCounter
+                  FloatingActionButton(
+              onPressed: _incrementCounter,
+              child: const Icon(Icons.add),
+            ),
+                  
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
-}
