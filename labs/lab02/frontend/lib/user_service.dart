fb4c5127abd3ec251b75a971465caf7aa950e74e
Timur <timyr20060430@gmail.com>
2025-07-03 19:56:13 +0300

    add user_profile.dart changes
    add method loadUser for getting user's date
    add screen elements

--- labs/lab02/frontend/lib/user_service.dart
+++ labs/lab02/frontend/lib/user_service.dart
@@ -1,7 +1,7 @@
 class UserService {
   Future<Map<String, String>> fetchUser() async {
     // TODO: Simulate fetching user data for tests
-    await Future.delayed(Duration(seconds: 1));
+    await Future.delayed(Duration(microseconds: 10));
     return {'name': 'Alice', 'email': 'alice@example.com'};
   }
 }
