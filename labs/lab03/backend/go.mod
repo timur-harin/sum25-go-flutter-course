84d36474d6b3ae33abc3d4396611e7c82769d148
ph1larmon1a <a.fominykh@innopolis.university>
2025-07-04 19:56:30 +0300

    Implement backend functionality

--- labs/lab03/backend/go.mod
+++ labs/lab03/backend/go.mod
@@ -3,5 +3,6 @@ module lab03-backend
 go 1.24
 
 require (
-	github.com/gorilla/mux v1.8.0
-) 
+	github.com/gogo/protobuf v1.3.2
+	github.com/gorilla/mux v1.8.1
+)
--- labs/lab03/backend/main.go
+++ labs/lab03/backend/main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"lab03-backend/api"
+	"lab03-backend/storage"
 	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-	// TODO: Create a new memory storage instance
-	// TODO: Create a new API handler with the storage
-	// TODO: Setup routes using the handler
-	// TODO: Configure server with:
-	//   - Address: ":8080"
-	//   - Handler: the router
-	//   - ReadTimeout: 15 seconds
-	//   - WriteTimeout: 15 seconds
-	//   - IdleTimeout: 60 seconds
-	// TODO: Add logging to show server is starting
-	// TODO: Start the server and handle any errors
+	var _memoryStorage = storage.NewMemoryStorage()
+	var _handler = api.NewHandler(_memoryStorage)
+	var _mux = _handler.SetupRoutes()
 
-	log.Println("TODO: Implement main function")
+	var server = &http.Server{
+		Addr:         ":8080",
+		Handler:      _mux,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	log.Println("Server is starting on http://localhost:8080")
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf(" Server failed to start: %v", err)
+	}
 }
