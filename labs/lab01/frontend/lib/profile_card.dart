3968e3cdd7f6464636581776ec67fcbd5e99ce0e
Your Name <you@example.com>
2025-07-09 20:50:23 +0300

    git add .

--- labs/lab01/frontend/lib/profile_card.dart
+++ labs/lab01/frontend/lib/profile_card.dart
@@ -16,22 +16,22 @@ class ProfileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(16.0),
+    return const Card(
+      margin: EdgeInsets.all(16.0),
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             // TODO: add a CircleAvatar with radius 50 and backgroundImage NetworkImage(avatarUrl!) if url is not null and text name[0].toUpperCase() if url is null
             
-            const SizedBox(height: 16),
+            SizedBox(height: 16),
             // TODO: add a Text with name and style fontSize: 24, fontWeight: FontWeight.bold
            
-            const SizedBox(height: 8),
+            SizedBox(height: 8),
             // TODO: add a Text with Age: $age and style fontSize: 16
            
-            const SizedBox(height: 8),
+            SizedBox(height: 8),
             // TODO: add a Text with email and style fontSize: 16, color: Colors.grey
             
           ],
