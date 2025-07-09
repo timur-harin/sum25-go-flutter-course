6689690c20b24aa9cf3cd63d4320fed4e7dc8a2f
shokoladniyVzhuh <n.selezenev@mail.ru>
2025-07-09 07:17:59 +0300

    lab04: implement frontend part
    - Implement a frontend part with all necessary files except secure_storage_service.dart because of incorrect tests
    - Make all by my own with small advices from our American friend(ChatGPT)

--- labs/lab04/frontend/lib/main.dart
+++ labs/lab04/frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab04_frontend/services/database_service.dart';
 import 'services/preferences_service.dart';
 import 'screens/home_screen.dart';
 
