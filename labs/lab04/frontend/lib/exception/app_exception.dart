94cb81db601bd9d914bd84155a6ecd2f41102cf1
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-07-09 20:26:07 +0300

    Store all exceptions in one file

--- labs/lab04/frontend/lib/exception/app_exception.dart
+++ labs/lab04/frontend/lib/exception/app_exception.dart
@@ -9,3 +9,21 @@ class AppException implements Exception {
     return '$prefix$message';
   }
 }
+
+class UninitializedDatabaseException extends AppException {
+  UninitializedDatabaseException() : super("Database not initialized");
+}
+
+class UninitializedPreferencesException extends AppException {
+  UninitializedPreferencesException()
+      : super("Preferences service not initialized");
+}
+
+class PreferencesParseException extends AppException {
+  PreferencesParseException(String message)
+      : super(message, "Failed to parse JSON object for key ");
+}
+
+class UserNotFoundException extends AppException {
+  UserNotFoundException() : super("User not found");
+}
